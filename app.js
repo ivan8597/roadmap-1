18bd8dc2f63a98c22a5817ee8c2762de9fa0db49
ivan <ivan8597@yandex.ru>
2021-03-08 17:57:45 +0300

    task 3

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
+const User=require('./model/User')
 
-app.get('/', (req, res) => {
-  res.send('Server Index Page');
+app.get('/users', (req, res) => {
+  res.json({
+    items:User.list()
+  })
 });
+
+app.get('/users/:id', (req, res) => {
+    res.json({
+      item:User.getById(req.params.id)
+    })
+  });
+
+
 app.use(express.static('public'));
+
 app.listen(PORT, () => {
   console.log(`Server app listening at http://localhost:${PORT}`);
 });

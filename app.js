1c621873c1a36e928f2f72e17108da3e2ba9068a
ivan <ivan8597@yandex.ru>
2021-03-08 17:45:03 +0300

    task2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = 3001;
 app.get('/', (req, res) => {
   res.send('Server Index Page');
 });
-
+app.use(express.static('public'));
 app.listen(PORT, () => {
   console.log(`Server app listening at http://localhost:${PORT}`);
 });

2efd8b7822f699f7414c656907c035bbc7ca632d
ivan <ivan8597@yandex.ru>
2021-03-15 18:59:12 +0300

    task 2.1-2.2

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 const User=require('./model/User')
+const Post=require("./model/Post")
+const Comment=require("./model/Comment")
+const cors = require('cors');
 
+app.use(cors());
+app.use(express.static('public'));
+
+/** USERS */
 app.get('/users', (req, res) => {
   res.json({
     items:User.list()
@@ -10,13 +17,37 @@ app.get('/users', (req, res) => {
 });
 
 app.get('/users/:id', (req, res) => {
-    res.json({
-      item:User.getById(req.params.id)
-    })
-  });
+  res.json({
+    item:User.getById(req.params.id)
+  })
+});
 
+/** POSTS */
+app.get('/posts', (req, res) => {
+  const userId=req.query.userId
+  res.json({
+    items:Post.list(userId)
+  })
+});
 
-app.use(express.static('public'));
+app.get('/posts/:id', (req, res) => {
+  res.json({
+    item:Post.getById(req.params.id)
+  })
+});
+/** Comments */
+app.get('/comments', (req, res) => {
+  const postId=req.query.postId
+  res.json({
+    items:Comment.list(postId)
+  })
+});
+
+app.get('/comments/:id', (req, res) => {
+  res.json({
+    item:Comment.getById(req.params.id)
+  })
+});
 
 app.listen(PORT, () => {
   console.log(`Server app listening at http://localhost:${PORT}`);

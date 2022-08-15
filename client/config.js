0349cfd2f76e3ede19d8b83bc65d84ab07beade4
naukomania <naukomania@gmail.com>
2022-08-15 08:12:36 +0300

    pre deploy

--- client/config.js
+++ client/config.js
@@ -1,2 +1,2 @@
 export const STORAGE_URL = process.env.NEXT_PUBLIC_STORAGE_URL || "http://localhost:3001/uploads";
-export const API_URL= process.env.NEXT_PUBLIC_STORAGE_URL || "http://localhost:3001";
+export const API_URL= process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
